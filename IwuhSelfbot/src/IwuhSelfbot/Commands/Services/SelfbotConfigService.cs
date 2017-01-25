2f66be15d341290edab58a2dff9f2cf854d74761
Matthew Faigan <matthew.faigan@gmail.com>
2017-01-24 19:50:48 -0500

    create config service

---
+++ IwuhSelfbot/src/IwuhSelfbot/Commands/Services/SelfbotConfigService.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IwuhSelfbot.Commands.Services
+{
+    public class SelfbotConfigService
+    {
+        public string Token { get; private set; }
+        public string Prefix { get; private set; }
+
+        /// <summary>
+        /// Creates a new instance, reading the token and prefix from the config file.
+        /// </summary>
+        public SelfbotConfigService()
+        {
+            JObject config = JObject.Parse(File.ReadAllText(@".\SelfbotConfig.json"));
+            Token = (string)config["Token"];
+            Prefix = (string)config["Prefix"];
+        }
+    }
+}
