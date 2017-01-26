b6766f5e2fb379344a9f031389bb072d55c4356a
Matthew Faigan <matthew.faigan@gmail.com>
2017-01-25 21:01:16 -0500

    move the get highest role colour logic to an extension method

--- IwuhSelfbot/src/IwuhSelfbot/Commands/Modules/FunModule.cs
+++ IwuhSelfbot/src/IwuhSelfbot/Commands/Modules/FunModule.cs
@@ -14,35 +14,12 @@ namespace IwuhSelfbot.Commands.Modules
         [Summary("Shows the input as if it's an action you're performing.")]
         public async Task MeCommand([Remainder, Summary("The action you're performing.")] string input)
         {
-            var guildUser = Context.User as SocketGuildUser;
-            Color embedColour; // Colour has a u, fight me irl
-            if (guildUser == null)
-            {
-                // If we get here it means we're in a DM, so just use the default colour.
-                embedColour = Color.Default;
-            }
-            else
-            {
-                // Convert each role ID to a role object, then order them by position in the hierarchy (highest first).
-                var roles = guildUser.RoleIds.Select(r => Context.Guild.GetRole(r)).OrderByDescending(r => r.Position);
-
-                // If none of the roles have a colour...
-                if (!roles.Any(r => !r.Color.Equals(Color.Default)))
-                {
-                    // ...use the default.
-                    embedColour = Color.Default;
-                }
-                else
-                {
-                    // Pick the highest role that has a colour, and use it.
-                    embedColour = roles.FirstOrDefault(r => !r.Color.Equals(Color.Default)).Color;
-                }
-            }
+            Color embedColour = Context.User.GetHighestRoleColor(Context.Guild);
 
             // Create the embed to use as the message.
             EmbedBuilder meEmbed = new EmbedBuilder()
                 .WithColor(embedColour)
-                .WithDescription($"{Format.Bold(guildUser?.Nickname ?? Context.User.Username)} {Format.Italics(input)}");
+                .WithDescription($"{Format.Bold((Context.User as SocketGuildUser)?.Nickname ?? Context.User.Username)} {Format.Italics(input)}");
 
             // Delete the command message.
             await Context.Message.DeleteAsync();
---
+++ IwuhSelfbot/src/IwuhSelfbot/SelfbotExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IwuhSelfbot
+{
+    public static class SelfbotExtensions
+    {
+        public static Color GetHighestRoleColor(this IUser user, IGuild guild)
+        {
+            var guildUser = user as SocketGuildUser;
+            Color embedColour; // Colour has a u, fight me irl
+            if (guildUser == null)
+            {
+                // If we get here it means we're in a DM, so just use the default colour.
+                embedColour = Color.Default;
+            }
+            else
+            {
+                // Convert each role ID to a role object, then order them by position in the hierarchy (highest first).
+                var roles = guildUser.RoleIds.Select(r => guild.GetRole(r)).OrderByDescending(r => r.Position);
+
+                // If none of the roles have a colour...
+                if (!roles.Any(r => !r.Color.Equals(Color.Default)))
+                {
+                    // ...use the default.
+                    embedColour = Color.Default;
+                }
+                else
+                {
+                    // Pick the highest role that has a colour, and use it.
+                    embedColour = roles.FirstOrDefault(r => !r.Color.Equals(Color.Default)).Color;
+                }
+            }
+
+            return embedColour;
+        }
+    }
+}
